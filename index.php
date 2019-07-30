399d855d94c1f6254aa58f048477aa0ebe2688a4
Ruth Prudence <ruth@ruthprudence.com>
2019-07-30 16:01:36 -0700

    split up the table into a separate file (table.php)

--- index.php
+++ index.php
@@ -14,86 +14,8 @@
 </head>
 <body>
   <h3>Mortgage Calculator</h3>
-  <table>
-    <div class="form">
-        <form id="form" class="form" action="" onsubmit="return false;" method="post">
-                  <tr>
-                    <td>
-                        <label for="purchasePrice">Purchase Price ($)</label>
-                    </td>
-                    <td>
-                        <input id="purchasePrice" type="number" step="1000" name="purchasePrice" class="field right" placeholder="What's the purchase price?" value="200000"/>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <label for="percentage">Down Payment (%)</label>
-                    </td>
-                    <td>
-                      <input min="0" max="100" id="percentage" name="percentage" type="number" step="1"  class="field right" value="20"/>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <label for="downPayment">Amount ($)</label>
-                    </td>
-                    <td>
-                      <input name="downPayment" class="field" id="downPayment"  type="number" step="100" name="downPayment" class="field right"/>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <label for="mortgageAmount">Mortgage Amount ($)</label>
-                    </td>
-                    <td>
-                      <input name="mortgageAmount" class="field" id="mortgageAmount"  type="number" step=".01" name="mortgageAmount" class="field right"/>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <label for="interestRate">Interest Rate (%)</label>
-                    </td>
-                    <td>
-                      <input min="0" max="100" id="interestRate" name="interestRate"  type="number" step=".01" class="field right" value="5"/>
-                    </td>
-                  <tr>
-                    <td>
-                      <label for="period">Period (years)</label>
-                    </td>
-                    <td>
-                      <input type="number" min="0" max="40" id="period" name="period"  type="number" step="1" class="field right" value="15"/>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <label for="totalCost">Total Cost ($)</label>
-                    </td>
-                    <td>
-                      <input id="totalCost"  type="number" step=".01" name="totalCost" class="field right"/>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <label for="monthlyPayment">Monthly Payment ($)</label>
-                    </td>
-                    <td>
-                      <!-- <div class="dollar"> -->
-                        <input id="monthlyPayment" type="number" step=".01"  name="monthlyPayment" class="field right"/>
-                      <!-- </div> -->
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <input type="submit" name="Calculate" id="submit"/>
-                    </td>
-                    <td>
-                      <button name="clear" id="clear">Reset</button>
-                    </td>
-                  </tr>
-          </table>
-        </form>
-    </div>
-  </table>
+
+  <?php include('table.php'); ?>
 </body>
 <script>
 
@@ -128,8 +50,16 @@ $(document).ready(function(){
 
       $( "#totalCost" ).val(returnTotalCost(c,L,n).toFixed(2));
 
+      // var x = $('#monthlyPayment').val();
+      // $('#monthlyPayment').val(addCommas(x));
     });
 });//end of jquery
+
+  // function addCommas(x) {
+  //   var parts = x.toString().split(".");
+  //   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  //   return parts.join(".");
+  // }
 
   function returnTotalCost(r, P, N){
     let total = 0;
@@ -163,8 +93,13 @@ $(document).ready(function(){
       {
           price = parseInt(quantity.value);
       }
+      // $(".numbers").each(function() {
+      //     $(this).format({format:"#,###", locale:"us"});
+      // });
   return price;
   }
+
+
 
   function getPercentage()
   {
