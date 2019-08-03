02f7f8e7adf7fbf172e8ad0a42aff069d73e5f83
Ruth Prudence <ruth@ruthprudence.com>
2019-08-03 09:29:53 -0700

    re-arranged table to display results below the inputs, as opposed to the right

--- table.php
+++ table.php
@@ -17,7 +17,7 @@
               <label for="purchasePrice">Purchase Price ($)</label>
             </td>
             <td>
-                <input id="purchasePrice" type="number" step="1000" name="purchasePrice" class="field right number" placeholder="What's the purchase price?" value="200000"/>
+                <input id="purchasePrice" type="number" step="1000" name="purchasePrice" class="field right number" placeholder="What's the purchase price?" value="400000"/>
             </td>
           </tr>
           <tr>
@@ -27,8 +27,6 @@
             <td>
               <input min="0" max="100" id="percentage" name="percentage" type="number" step="1"  class="field right" value="20"/>
             </td>
-            <td class="label">Down Payment</td>
-            <td class="output"><span name="downPayment" class="field number right" id="downPayment" name="downPayment" class="field right"></span/></td>
           </tr>
             <tr>
               <td>
@@ -36,11 +34,6 @@
               </td>
               <td>
                 <input min="0" max="100" id="interestRate" name="interestRate"  type="number" step=".01" class="field right" value="5"/>
-              </td>
-              <td class="label">
-                Loan
-              </td>
-              <td class="output"><span name="mortgageAmount" class="field number right" id="mortgageAmount"  type="number" step=".01" name="mortgageAmount" class="field right"></span>
               </td>
             </tr>
             <tr>
@@ -50,6 +43,27 @@
               <td>
                 <input type="number" min="0" max="40" id="period" name="period"  type="number" step="5" class="field right" value="15"/>
               </td>
+            </tr>
+            <tr>
+              <td>
+                <input type="submit" name="Calculate" id="submit" class="button"/>
+              </td>
+              <td>
+                <button name="clear" id="clear" class="button">Reset</button>
+              </td>
+            </tr>
+            <tr>
+              <td class="label">Down Payment</td>
+              <td class="output"><span name="downPayment" class="field number right" id="downPayment" name="downPayment" class="field right"></span/></td>
+            </tr>
+            <tr>
+              <td class="label">
+                Loan
+              </td>
+              <td class="output"><span name="mortgageAmount" class="field number right" id="mortgageAmount"  type="number" step=".01" name="mortgageAmount" class="field right"></span>
+              </td>
+            </tr>
+            <tr>
               <td class="label">
                 Mortgage
               </td>
@@ -58,22 +72,9 @@
               </td>
             </tr>
             <tr>
-
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
               <td class="label">Monthly Payment</td>
               <td class="output">
                 <span id="monthlyPayment" name="monthlyPayment" class="field right numbers"></span>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <input type="submit" name="Calculate" id="submit" class="button"/>
-              </td>
-              <td>
-                <button name="clear" id="clear" class="button">Reset</button>
               </td>
             </tr>
       </form>
